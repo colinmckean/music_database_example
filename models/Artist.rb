60e88dd7211338c8d5bd3b2f56d0626eedcc7010
Colin McKean <colinmckeandigital@gmail.com>
2017-01-25 15:02:50 +0000

    have working save function and data to store in db

--- models/Artist.rb
+++ models/Artist.rb
@@ -5,7 +5,12 @@ class Artist
 
   attr_accessor :id, :name
   def initialize(options)
-    
+    @id = options['id']
+    @name = options['name']
   end
-
+  def save()
+    sql = "INSERT INTO artists(name) VALUES ('#{@name}')returning*;"
+    result = SqlRunner.run(sql)
+    @id = result[0]["id"].to_i
+  end
 end
---
+++ models/console.rb
@@ -0,0 +1,24 @@
+require 'pry-byebug'
+require_relative 'Album'
+require_relative 'Artist'
+
+artist_1 = Artist.new('name' => 'ED SHEERAN')
+artist_2 = Artist.new({'name' => 'Beyonce'})
+artist_1.save
+artist_2.save
+
+album_1 = Album.new({'title' => 'songs I wrote with amy', 'genre' => 'acoustic music' , 'artist_id'=> artist_1.id})
+album_2 = Album.new({'title' =>'LEMONADE', 'genre' => 'RnB', 'artist_id' =>  artist_2.id})
+
+album_3 = Album.new({'title' => 'Dangerously in Love', 'genre' => 'RnB', 'artist_id' => artist_2.id})
+album_4 = Album.new({'title' => 'Loose Change', 'genre' => 'acoustic music' , 'artist_id'=> artist_1.id})
+album_5 = Album.new({'title' => 'the slumdon bridge', 'genre' => 'acoustic music' , 'artist_id'=> artist_1.id})
+album_1.save
+album_2.save
+album_3.save
+album_4.save
+album_5.save
+
+binding.pry
+
+nil
